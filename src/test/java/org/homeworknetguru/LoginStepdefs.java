ddf85c4c08934a834929f3e180657e9ef421786d
Tomasz Galik <tomasz.galik@stepstone.com>
2018-12-17 14:32:26 +0100

    Initial commit

---
+++ src/test/java/org/homeworknetguru/LoginStepdefs.java
@@ -0,0 +1,101 @@
+package org.homeworknetguru;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertNotNull;
+
+public class LoginStepdefs {
+
+    WebDriver driver;
+
+    @Given("^I am on the main page https://trello.com/$")
+    public void I_am_on_the_main_page_https_trello_com() throws Throwable {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://trello.com");
+    }
+
+    @And("^I go to Login Page$")
+    public void I_go_to_Login_Page() throws Throwable {
+        driver.findElement(By.xpath("//a[@href ='/login?returnUrl=%2F']")).click();
+    }
+
+
+    @And("^I input correct email address$")
+    public void I_input_correct_email_address() throws Throwable {
+//        driver.findElement(By.id("user")).click();
+        driver.findElement(By.cssSelector(".hide-when-two-factor #user")).sendKeys("testytomka@gmail.com");
+
+    }
+
+    @And("^I input password$")
+    public void I_input_password() throws Throwable {
+        driver.findElement(By.cssSelector(".hide-when-two-factor #password")).sendKeys("homeworkng");
+    }
+
+    @When("^I click “Log in”$")
+    public void I_click_Log_in() throws Throwable {
+        driver.findElement(By.id("login")).click();
+    }
+
+    @Then("^I am logged in$")
+    public void I_am_logged_in() throws Throwable {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".header-user")));
+        assertNotNull(element);
+    }
+
+    @And("^I am on dashboard page$")
+    public void I_am_on_dashboard_page() throws Throwable {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("home-left-sidebar-container")));
+        assertNotNull(element);
+        driver.close();
+    }
+
+    @And("^I click “Sign up” button$")
+    public void I_click_Sign_up_button() throws Throwable {
+        driver.findElement(By.xpath("//a[@href ='/signup?returnUrl=%2F']")).click();           }
+
+    @And("^I click “Sign in to your account”$")
+    public void I_click_Sign_in_to_your_account() throws Throwable {
+       driver.findElement(By.cssSelector(".sign-in-account a")).click();
+    }
+
+
+    @And("^I input incorrect password$")
+    public void I_input_incorrect_password() throws Throwable {
+        driver.findElement(By.cssSelector(".hide-when-two-factor #password")).sendKeys("1homeworkng");
+
+    }
+
+    @Then("^I get prompt message “Invalid password”$")
+    public void I_get_prompt_message_Invalid_password() throws Throwable {
+        WebElement error = driver.findElement(By.xpath("//p[contains(@class, 'error-message')]"));
+        assertNotNull(error);
+        driver.close();
+    }
+
+    @And("^I input incorrect email address$")
+    public void I_input_incorrect_email_address() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^I get prompt message “There isn't an account for this email”$")
+    public void I_get_prompt_message_There_isn_t_an_account_for_this_email() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+}
---
+++ src/test/resources/login.feature
@@ -0,0 +1,38 @@
+Feature: User login
+
+Scenario: User logs in with email and password
+  Given I am on the main page https://trello.com/
+  And I go to Login Page
+  And I input correct email address
+  And I input password
+  When I click “Log in”
+  Then I am logged in
+  And I am on dashboard page
+
+Scenario: User login from registration page
+
+  Given I am on the main page https://trello.com/
+  And I click “Sign up” button
+  And I click “Sign in to your account”
+  And I input correct email address
+  And I input password
+  When I click “Log in”
+  Then I am logged in
+  And I am on dashboard page
+
+Scenario: User try to log in with wrong password
+
+  Given I am on the main page https://trello.com/
+  And I go to Login Page
+  And I input correct email address
+  And I input incorrect password
+  When I click “Log in”
+  Then I get prompt message “Invalid password”
+
+#Scenario: User try to log in with wrong email
+#Given I am on the main page https://trello.com/
+#And I go to Login Page
+#And I input incorrect email address
+#And	I input correct password
+#When I click “Log in”
+#Then I get prompt message “There isn't an account for this email”
